34853f8908a2bb25c028df96d1594d36191cb262
gparmigiani <gparmigiani@travix.com>
2024-08-19 17:17:36 +0200

    java agent

--- AspireTest.AppHost/Program.cs
+++ AspireTest.AppHost/Program.cs
@@ -8,22 +8,23 @@ builder.AddProject<Projects.AspireTest_Web>("webfrontend")
 
 // Add the Spring Boot Docker container
 // docker run -it -p 9099:8888 aspirejava:0.0.1-SNAPSHOT
-builder.AddContainer("my-springboot-app", "aspirejava", "0.0.1-SNAPSHOT")
+builder.AddContainer("my-springboot-app", "aspirejava", "latest")
     .WithEndpoint(
         port: 9099,
         targetPort: 8080, 
         scheme: "http",
         name: "web")
-    .WithEndpoint(
-        port: 4317,
-        targetPort: 4317, 
-        scheme: "http",
-        name: "grpc-otel")
-    .WithEndpoint(
-        port: 4318,
-        targetPort: 4318, 
-        scheme: "http",
-        name: "http-otel")
+    // .WithEndpoint(
+    //     port: 18889,
+    //     targetPort: 4317, 
+    //     scheme: "http",
+    //     name: "grpc-otel")
+    // .WithEndpoint(
+    //     port: 18890,
+    //     targetPort: 4318, 
+    //     scheme: "http",
+    //     name: "http-otel")
     .WithOtlpExporter();
+
 
 builder.Build().Run();
